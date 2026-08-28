280fc3512dc14a801ffcb582eaf26c192c0e201d
M.Irfan <f4nzamv05@gmail.com>
2026-08-28 12:55:02 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,13 @@
-// Replace the three placeholder values below with your Firebase project details.
+// Firebase web configuration for Majlis Bayangan.
 // The Overseer password is NEVER stored in this file.
 window.MB_FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_WEB_API_KEY",
-  databaseURL: "YOUR_REALTIME_DATABASE_URL",
-  adminEmail: "YOUR_OVERSEER_EMAIL",
+  apiKey: "AIzaSyAt0W2-2ouraShlWqMEj4Li_We6Q26-acU",
+  authDomain: "majlisbayangan.firebaseapp.com",
+  databaseURL: "https://majlisbayangan-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "majlisbayangan",
+  storageBucket: "majlisbayangan.firebasestorage.app",
+  messagingSenderId: "653943729807",
+  appId: "1:653943729807:web:ca8a2ee05614e197694afa",
+  adminEmail: "f4nzamv05@gmail.com",
   overseerName: "F4nz2005"
 };
-
